209c9b1449ddebf95acd15f7608193eeff6b2ad7
putyesz <putyesz10@gmail.com>
2019-06-28 17:50:06 +0200

    fix bootstrap

---
+++ Torpedo/DAL/Score.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Torpedo.DAL
+{
+    public class Score : DbContext
+    {
+        public Score() : base("Score")
+        {
+        }
+
+        public DbSet<Score>
+
+    }
+}
